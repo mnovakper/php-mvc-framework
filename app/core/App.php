e105b7e9fee7dd26293e72b934b5f8e6147da679
Matej <matej.dinamo@hotmail.com>
2023-03-03 19:51:55 +0100

    komm

--- app/core/App.php
+++ app/core/App.php
@@ -13,17 +13,17 @@ class App
         return $URL;
     }
 
-    // finding right controller file based on first item in array (splitURL) // pronalazak controller fajla na temelju prve stavke u nizu
+    // pronalazak controller fajla na temelju prve stavke u nizu
     public function loadController()
     {
         $URL = $this->splitURL();
 
-        // controller select // odabir controllera
+        // odabir controllera
         $filename = "../app/controllers/".ucfirst($URL[0]).".php";
         if(file_exists($filename))
         {
-            require $filename; // load controller file // ucitavanje controller fajla
-            $this->controller = ucfirst($URL[0]); // controller name // ime controllera
+            require $filename; // ucitavanje controller fajla
+            $this->controller = ucfirst($URL[0]); // ime controllera
             unset($URL[0]);
         } else {
             $filename = "../app/controllers/_404.php";
@@ -33,7 +33,7 @@ class App
 
         $controller = new $this->controller;
 
-        // method select // odabir metode (prvi param ime controller fajla, drugi param ime metode)
+        // odabir metode (prvi parametar je ime controller fajla, drugi param ime metode)
         if (!empty($URL[1]))
         {
             if (method_exists($controller, $URL[1]))
@@ -42,6 +42,6 @@ class App
                 unset($URL[1]);
             }
         }
-        call_user_func_array([$controller, $this->method], $URL); // first param is object (which controller to load), second param is method name to load // prvi param je objekt (controller koji ce se ucitati), a drugi je ime metode koja ce se ucitati
+        call_user_func_array([$controller, $this->method], $URL); // prvi param je objekt (controller koji ce se ucitati), a drugi je ime metode koja ce se ucitati
     }
 }
